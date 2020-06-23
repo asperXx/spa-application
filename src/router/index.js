5e938fed643f5aa3b38020282ebdc2000c131aff
Asper <varfol0077@gmail.com>
2020-06-23 15:17:10 +0300

    add components

--- src/router/index.js
+++ src/router/index.js
@@ -4,5 +4,42 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 export default new Router({
-    router: []
+    router: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../components/Home.vue')
+        },
+        {
+            path: '/ad/:id',
+            name: 'ad',
+            component: () => import('../components/Ads/Ad.vue')
+        },
+        {
+            path: '/list',
+            name: 'list',
+            component: () => import('../components/Ads/AdList.vue')
+        },
+        {
+            path: '/new',
+            name: 'new',
+            component: () => import('../components/Ads/NewAd.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../components/Auth/Login.vue')
+        },
+        {
+            path: '/registration',
+            name: 'reg',
+            component: () => import('../components/Auth/Registration.vue')
+        },
+        {
+            path: '/orders',
+            name: 'orders',
+            component: () => import('../components/User/Orders.vue')
+        },
+    ],
+    mode: 'history'
 });
